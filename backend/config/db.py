368d73f7df420c7f536cdecf3d9ce2cc56c02fc5
agrawaljay12 <agrawaljay247@gmail.com>
2026-04-16 16:56:20 +0530

    refactor: update MongoDB connection string to use Atlas cluster

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb+srv://agrawaljay247_db_user:<agrawaljay247_db_user>@cluster0.bdgppsl.mongodb.net/?appName=Cluster0')
 
 db = client['smart_local_services']
 
